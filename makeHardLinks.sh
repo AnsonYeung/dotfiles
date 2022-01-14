92751862c86f8132d056a7cbe7de55b1ec8692cf
Anson Yeung <s151204@tanghin.edu.hk>
2022-01-14 15:56:51 +0800

    Allow forwarding argument to makeHardLinks.sh

--- makeHardLinks.sh
+++ makeHardLinks.sh
@@ -3,6 +3,6 @@
 FILES=$(cat fileList.txt)
 mkdir -p ~/.config/nvim/UltiSnips
 for i in $FILES; do
-    ln $PWD/$i ~/$i
+    ln $PWD/$i ~/$i "$@"
 done
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
