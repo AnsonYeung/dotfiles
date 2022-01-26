d1c143030de9bc312d82ad1c684d3bee0eda9cc4
Anson Yeung <nanolify@gmail.com>
2022-01-27 01:23:10 +0800

    auto commit

--- makeHardLinks.sh
+++ makeHardLinks.sh
@@ -7,6 +7,10 @@ for i in $FILES; do
 done
 ln .profile ~/.zprofile "$@"
 ln .gdbinit.gef ~/.gdbinit "$@"
-if [ ! -d ~/powerlevel10k ]; then
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+#if [ ! -d ~/powerlevel10k ]; then
+#    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+#fi
+if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
+
