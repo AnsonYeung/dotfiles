76dc3ca0876b49f78f447633f4dac8117889fb54
Anson Yeung <nanolify@gmail.com>
2022-01-27 02:20:33 +0800

    auto commit

--- makeHardLinks.sh
+++ makeHardLinks.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# oh my zsh will overwrite files on install
+if [ ! -d ${ZSH:-$HOME/.oh-my-zsh} ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
 FILES=$(cat fileList.txt)
 for i in $FILES; do
     mkdir -p `dirname ~/$i`
@@ -9,9 +18,3 @@ done
 ln .profile ~/.zprofile "$@"
 ln .gdbinit.gef ~/.gdbinit "$@"
 
-if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom} ]; then
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/jeffreytse/zsh-vi-mode {ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-fi
