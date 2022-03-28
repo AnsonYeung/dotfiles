ebfac6ee4efe1c1eafff99199da6abf60704013c
Anson Yeung <nanolify@gmail.com>
2022-03-29 00:02:02 +0800

    Fix termux zsh bug

--- autoupdatelist.sh
+++ autoupdatelist.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 declare -A autoupdatelist
 ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
-autoupdatelist[${DOTFILES_PATH:-$HOME/github/dotfiles}]=""
+autoupdatelist[${DOTFILES_PATH:-$HOME/github/dotfiles}]="NULL"
 autoupdatelist[${ZSH:-$HOME/.oh-my-zsh}]="https://github.com/ohmyzsh/ohmyzsh.git"
 autoupdatelist[$ZSH_CUSTOM/themes/powerlevel10k]="https://github.com/romkatv/powerlevel10k.git"
 autoupdatelist[$ZSH_CUSTOM/plugins/zsh-autosuggestions]="https://github.com/zsh-users/zsh-autosuggestions.git"
