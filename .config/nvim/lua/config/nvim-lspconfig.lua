32aeebee57c2d63608a6bb0ccc34910de7d7fc1f
Anson Yeung <nanolify@gmail.com>
2022-08-24 17:33:53 +0800

    improve lua-language-server perf by not loading nvim runtime

--- .config/nvim/lua/config/nvim-lspconfig.lua
+++ .config/nvim/lua/config/nvim-lspconfig.lua
@@ -62,11 +62,11 @@ require 'lspconfig'.sumneko_lua.setup {
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
-                globals = { 'vim', 'ipairs' },
+                globals = { 'vim', 'ipairs', 'require' },
             },
             workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
+                -- Make the server aware of Neovim runtime files (perf issues)
+                -- library = vim.api.nvim_get_runtime_file("", true),
             },
             -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
