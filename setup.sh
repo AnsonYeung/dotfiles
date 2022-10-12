760381e822862666133ecbbf1b2dbbf8c91c680c
Anson Yeung <nanolify@gmail.com>
2022-10-12 23:39:14 +0800

    change to depth 1

--- setup.sh
+++ setup.sh
@@ -10,17 +10,17 @@ if [ ! -f ~/.gdbinit ]; then
 fi
 
 if [ ! -d ~/.tmux/plugins/tpm ]; then
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 if [ ! -d ~/.oh-my-zsh ]; then
-    git clone https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
+    git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
 fi
 
 source $SCRIPT_DIR/autoupdatelist.sh
 for plugpath url in ${(kv)autoupdatelist}; do
     if [ ! -d $plugpath ]; then
-        git clone $url $plugpath
+        git clone --depth=1 $url $plugpath
     fi
 done
 
