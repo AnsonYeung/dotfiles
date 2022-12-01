966b6d988bdf976e8e055103ec49f70a7376af48
Anson Yeung <nanolify@gmail.com>
2022-12-01 18:07:19 +0800

    adding math condition

--- .config/nvim/luasnippets/tex.lua
+++ .config/nvim/luasnippets/tex.lua
@@ -4,6 +4,10 @@ local f = ls.function_node
 local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 
+local function math()
+    return vim.api.nvim_eval('vimtex#syntax#in_mathzone()') == 1
+end
+
 return {
     ls.parser.parse_snippet({ trig = 'template' }, [[
 \documentclass[a4paper]{article}
@@ -30,15 +34,17 @@ $0
     ls.parser.parse_snippet({ trig = 'beg' }, '\\begin{$1}\n$2\n\\end{$1}\n'),
     ls.parser.parse_snippet({ trig = 'ali' }, '\\begin{align*}\n$1\n\\end{align*}\n'),
     ls.parser.parse_snippet({ trig = 'enum' }, '\\begin{enumerate}\n$1\n\\end{enumerate}\n'),
-    ls.parser.parse_snippet({ trig = 'sr', wordTrig = false }, '^2'),
-    ls.parser.parse_snippet({ trig = 'cb', wordTrig = false }, '^3'),
-    ls.parser.parse_snippet({ trig = 'td', wordTrig = false }, '^{$1}'),
-    ls.parser.parse_snippet({ trig = 'sq' }, '\\sqrt{$1}'),
-    ls.parser.parse_snippet({ trig = 'vec' }, '\\vec{$1}'),
+    ls.parser.parse_snippet({ trig = 'sr', wordTrig = false }, '^2', { condition = math }),
+    ls.parser.parse_snippet({ trig = 'cb', wordTrig = false }, '^3', { condition = math }),
+    ls.parser.parse_snippet({ trig = 'td', wordTrig = false }, '^{$1}', { condition = math }),
+    ls.parser.parse_snippet({ trig = 'sq' }, '\\sqrt{$1}', { condition = math }),
+    ls.parser.parse_snippet({ trig = 'vec' }, '\\vec{$1}', { condition = math }),
 
-    s({ trig = '([%d%a\\_]+)/', regTrig = true }, fmt([[\frac{<>}{<>}]], { f(
-        function(_, snip)
-            return snip.captures[1]
-        end
-    ), i(1) }, { delimiters = '<>' })),
+    s({ trig = '([%d%a\\_]+)/', regTrig = true },
+        fmt([[\frac{<>}{<>}]], { f(
+            function(_, snip)
+                return snip.captures[1]
+            end
+        ), i(1) }, { delimiters = '<>' }),
+        { condition = math }),
 }
