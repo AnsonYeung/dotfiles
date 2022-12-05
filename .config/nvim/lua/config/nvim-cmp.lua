6f3db4f8cb21f0ffc29ff22307f9ac5941e0267c
Anson Yeung <nanolify@gmail.com>
2022-12-05 17:03:15 +0800

    keymap for select mode jumping

--- .config/nvim/lua/config/nvim-cmp.lua
+++ .config/nvim/lua/config/nvim-cmp.lua
@@ -61,6 +61,13 @@ cmp.setup({
                     fallback()
                 end
             end,
+            s = function(fallback)
+                if luasnip.jumpable(1) then
+                    luasnip.jump(1)
+                else
+                    fallback()
+                end
+            end,
             c = function(fallback)
                 cmp.select_next_item()
             end
@@ -70,6 +77,13 @@ cmp.setup({
                 if cmp.visible() then
                     cmp.select_prev_item()
                 elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end,
+            s = function(fallback)
+                if luasnip.jumpable(-1) then
                     luasnip.jump(-1)
                 else
                     fallback()
