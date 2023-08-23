0de25ec61eaec355edf1a0f5a7488400d567e574
Anson Yeung <nanolify@gmail.com>
2023-08-23 19:31:53 +0800

    Disable treesitter on large files as it causes performance issues.

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -13,7 +13,25 @@ require('nvim-treesitter.configs').setup {
         enable = true,
 
         -- list of language that will be disabled
-        disable = { "latex" },
+        disable = function(lang, buf)
+            if lang == "latex" then
+                return true
+            end
+
+            -- local max_filesize = 100 * 1024 -- 100 KB
+            -- local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            -- if ok and stats and stats.size > max_filesize then
+            --     return true
+            -- end
+
+            local max_line = 4096
+            local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+            for _, line in ipairs(lines) do
+                if #line > max_line then
+                    return true
+                end
+            end
+        end,
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
