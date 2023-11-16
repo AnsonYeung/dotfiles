0fe19d3c6c26a84ac3045a756d1bf2f6bcebf017
Anson Yeung <nanolify@gmail.com>
2023-11-16 17:19:01 +0800

    switched to pwndbg

--- gitdir_list.sh
+++ gitdir_list.sh
@@ -7,6 +7,7 @@ gitdir_add ${ZSH:-$HOME/.oh-my-zsh} https://github.com/ohmyzsh/ohmyzsh.git
 gitdir_add $ZSH_CUSTOM/themes/powerlevel10k https://github.com/romkatv/powerlevel10k.git
 gitdir_add $ZSH_CUSTOM/plugins/zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions.git
 gitdir_add $ZSH_CUSTOM/plugins/zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting.git
+gitdir_add $HOME/github/pwndbg https://github.com/pwndbg/pwndbg
 
 if [ -f $HOME/.extra_gitdir.sh ]; then
     source $HOME/.extra_gitdir.sh
--- setup.sh
+++ setup.sh
@@ -4,11 +4,6 @@ ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 SCRIPT_DIR=$(realpath "$(dirname $0)")
 FILES=$(cat $SCRIPT_DIR/file_list.txt)
 
-if [ ! -f ~/.gdbinit ]; then
-    curl -fsSLo ~/.gdbinit-gef.py http://gef.blah.cat/py
-    echo source ~/.gdbinit-gef.py >> ~/.gdbinit
-fi
-
 gitdir_add() {
     if [ ! -d $1 ]; then
         git clone --filter=blob:none $2 $1
@@ -16,6 +11,10 @@ gitdir_add() {
 }
 
 source $SCRIPT_DIR/gitdir_list.sh
+
+if [ ! -f ~/.gdbinit ]; then
+    ~/github/pwndbg/setup.sh
+fi
 
 for i in ${(f)FILES}; do
     mkdir -p $(dirname ~/$i)
